8f471f11e73a69fdaabd2c99faaf062bc27a5a88
ingridevv <personnel.ingrid@gmail.com>
2025-09-16 17:22:31 -0300

    feat(footer): set responsive layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,50 +6,57 @@ function Footer() {
     <>
       <Newsletter />
 
-      <footer className="bg-[#FBF6F0] grid grid-cols-4 gap-10 mx-50">
-        <div className="m-10">
+      <footer className="bg-[#FBF6F0] px-4 md:px-20 py-10">
+        {/* Top row: Whiskers | Suporte */}
+        <div className="grid grid-cols-2 gap-10">
+          <div>
             <ul>
-                <li className="my-4"><span className="opacity-60">Whiskers</span></li>
-                <li className="my-4">Sobre Nós</li>
-                <li className="my-4">Carreiras</li>
-                <li className="my-4">Imprensa</li>
+              <li className="my-4">
+                <span className="opacity-60">Whiskers</span>
+              </li>
+              <li className="my-4">Sobre Nós</li>
+              <li className="my-4">Carreiras</li>
+              <li className="my-4">Imprensa</li>
             </ul>
-        </div>
-        
-        <div className="m-10">
+          </div>
+          <div>
             <ul>
-                <li className="my-4"><span className="opacity-60">Suporte</span></li>
-                <li className="my-4">Perguntas Frequentes</li>
-                <li className="my-4">Blog</li>
-                <li className="my-4">Contate-nos</li>
+              <li className="my-4">
+                <span className="opacity-60">Suporte</span>
+              </li>
+              <li className="my-4">Perguntas Frequentes</li>
+              <li className="my-4">Blog</li>
+              <li className="my-4">Contate-nos</li>
             </ul>
+          </div>
         </div>
-        
-        <div className="m-10">
+        {/* Bottom row: Legal | Siga-nos */}
+        <div className="grid grid-cols-2 gap-10 mt-8">
+          <div>
             <ul>
-                <li className="my-4"><span className="opacity-60">Legal</span></li>
-                <li className="my-4">Política de Privacidade</li>
-                <li className="my-4">Termos & Condições</li>
-                <li className="my-4">Reembolso</li>
+              <li className="my-4">
+                <span className="opacity-60">Legal</span>
+              </li>
+              <li className="my-4">Política de Privacidade</li>
+              <li className="my-4">Termos & Condições</li>
+              <li className="my-4">Reembolso</li>
             </ul>
-        </div>
-
-        <div className="m-10">
+          </div>
+          <div>
             <ul>
-                <li className="my-4"><span className="opacity-60">Siga-nos</span></li>
-                <li className="my-4">Facebook</li>
-                <li className="my-4">Instagram</li>
-                <li className="my-4">LinkedIn</li>
-                <li className="my-4">X</li>
+              <li className="my-4">
+                <span className="opacity-60">Siga-nos</span>
+              </li>
+              <li className="my-4">Facebook</li>
+              <li className="my-4">Instagram</li>
+              <li className="my-4">LinkedIn</li>
+              <li className="my-4">X</li>
             </ul>
+          </div>
         </div>
-
-        
       </footer>
 
-
-
-    {/* All Rights Reserved */}
+      {/* All Rights Reserved */}
       <div className="flex flex-col items-center m-10">
         <span className="text-2sm text-[#161C2D] opacity-70">
           © 2025 Whiskers. Todos os direitos reservados | por Ingrid Silva
